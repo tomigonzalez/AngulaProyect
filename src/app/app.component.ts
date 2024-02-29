299676fcca01ea723076b1843339d54ced7de087
Tomas Gonzalez <tomimateo2008@hotmail.com>
2024-02-29 01:44:20 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { SobrenosotrosComponent } from './sobrenosotros/sobrenosotros.component';
+import { CocktailsComponent } from './cocktails/cocktails.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavComponent,
+    HomeComponent,
+    SobrenosotrosComponent,
+    CocktailsComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-cocktails';
---
+++ src/app/cocktails/cocktails.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cocktails',
+  standalone: true,
+  imports: [],
+  templateUrl: './cocktails.component.html',
+  styleUrl: './cocktails.component.css'
+})
+export class CocktailsComponent {
+
+}
